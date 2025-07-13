64c4edf9b3799466e220808c4f9647576c8c39a4
Toluwase Ayomide <167195927+Toludayo75@users.noreply.github.com>
2025-07-13 22:30:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -71,10 +71,5 @@ def create_app():
     
     return app
 
-# Create app instance
-def create_app():
-    app = Flask(__name__, static_folder='static')  # <--- updated line
-    ...
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
